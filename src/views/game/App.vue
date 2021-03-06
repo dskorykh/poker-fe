9e243600dfb8a317d119bdc3b3831545918aeed0
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-03-06 19:18:13 +0300

    src: remove router & init layout

--- src/views/game/App.vue
+++ src/views/game/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <PokerHello/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import PokerHello from '@/components/PokerHello.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    PokerHello
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/views/index/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <PokerHello/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PokerHello from '@/components/PokerHello.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    PokerHello
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
