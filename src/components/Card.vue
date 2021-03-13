b6321f17679df5fe9f49b6384fe32d57fd4dc476
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-03-14 01:46:53 +0300

    src: components: game: initial

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button>{{ value }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isSelected: false
+    }
+  },
+  props: {
+    value: String
+  }
+}
+</script>
+
+<style>
+
+</style>
