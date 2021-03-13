a7f97643e51be5d69b792f8947d498fd17ec7684
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-03-13 20:34:22 +0300

    vue-config: add dev mode features and paths for prod

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,24 @@
 const titles = require('./title.js')
 const glob = require('glob')
 const pages = {}
+const path = require('path')
+const cli = require('commander');
+
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+const staticFolder = IS_PROD ? '../static/' : '';
+const cssFolder = IS_PROD ? `${staticFolder}css/` : './css/';
+const imagesFolder = IS_PROD ? `${staticFolder}img/` : './images/';
+const assetsDir = IS_PROD ? '../static' : '';
+
+cli
+  .option('-i, --ip <ip>', 'Device IP used along development.', '192.168.2.15')
+  .parse(process.argv);
+
+const options = cli.opts();
+
+console.log(options.ip);
+process.env.VUE_APP_IP = options.ip;
 
 glob.sync('./src/views/**/app.js').forEach(path => {
   const chunk = path.split('./src/views/')[1].split('/app.js')[0]
@@ -9,19 +27,34 @@ glob.sync('./src/views/**/app.js').forEach(path => {
     entry: path,
     template: 'public/index.html',
     title: titles[chunk],
-    chunks: ['chunk-vendors', 'chunk-common', chunk]
+    chunks: ['chunk-vendors', 'chunk-common', chunk],
+    IS_PROD,
+    staticFolder,
+    cssFolder,
+    imagesFolder
   }
 })
 module.exports = {
+  outputDir: '../templates',
+  assetsDir,
   pages,
-  chainWebpack: config => config.plugins.delete('named-chunks'),
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
-      }
-    }
-  }
+  chainWebpack: config => {
+    config.resolve.alias.set(
+      'vue$',
+      // If using the runtime only build
+      path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
+      // Or if using full build of Vue (runtime + compiler)
+      // path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
+    );
+    config.plugins.delete('named-chunks');
+  },
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://127.0.0.1:8080',
+  //       changeOrigin: true,
+  //       pathRewrite: { '^/api': '' }
+  //     }
+  //   }
+  // }
 }
