0793fe7795c841539d9f22712cdf9c7e682a0183
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2021-03-21 21:15:15 +0300

    src: views: game: store: add more handlers for REST API

--- src/views/game/store/index.js
+++ src/views/game/store/index.js
@@ -8,28 +8,51 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    playersList: ['zxczxc', 'zxcxzczxc', 'ddfdgdfg', 'zxczxc', 'zxcxzczxc', 'ddfdgdfg','zxczxc', 'zxcxzczxc', 'ddfdgdfg','zxczxc', 'zxcxzczxc', 'ddfdgdfg','zxczxc', 'zxcxzczxc', 'ddfdgdfg', 'zxczxc', 'zxcxzczxc', 'ddfdgdfg',],
+    playersList: [],
     playerName: '',
     isVoteActive: false,
+    isVoteCompleted: false,
     voteStatistics: {
       average: 0,
       votes: {}
     },
     isVoteSet: false,
-    sessionId: '3f8ec9aa798'
+    sessionId: '70ee9841b84',
+    completedVotes: {},
+    roomName: '',
+    activeVoteTitle: ''
   },
   mutations: {
+    setSessionId: (state, id) => {
+      state.sessionId = id;
+    },
     setPlayersList: (state, payload) => {
       state.playersList = payload;
     },
     setVotes: (state, payload) => {
-      state.voteStatistics.votes = payload;
+      state.voteStatistics.votes = Object.assign({}, state.voteStatistics.votes, payload);
+    },
+    clearVoteStats: (state) => {
+      state.voteStatistics.votes = {};
     },
     setVoteActive: (state, payload) => {
       state.isVoteActive = payload;
     },
-    setVoteStats: (state, payload) => {
-      state.voteStatistics = payload;
+    setVoteCompleted: (state, payload) => {
+      state.isVoteCompleted = payload;
+    },
+    setVoteStats: (state, stats) => {
+      const title = stats.title;
+      delete stats.title;
+      state.voteStatistics = stats;
+      const payload = {
+        [title]: {
+          'average': stats.average,
+          'votes': stats.votes,
+        }
+      }
+      console.log(payload);
+      state.completedVotes = Object.assign({}, state.completedVotes, payload);
     },
     setVoteSet: (state, payload) => {
       state.isVoteSet = payload;
@@ -37,13 +60,19 @@ export default new Vuex.Store({
     setPlayerName: (state, name) => {
       state.playerName = name;
     },
+    setRoomName: (state, name) => {
+      state.roomName = name;
+    },
+    setActiveVoteTitle: (state, title) => {
+      state.activeVoteTitle = title;
+    },
 
   },
   actions: {
     getAllPlayers({ commit, state }) {
       xhr.get(`/${state.sessionId}/users`)
         .then(({ data }) => {
-          commit('setPlayersList', data.all_members);
+          commit('setPlayersList', data.session_users);
         })
     },
     getAllVotes({ commit, state }) {
@@ -53,22 +82,59 @@ export default new Vuex.Store({
         })
     },
     endVote({ commit, state }) {
-      xhr.get(`${state.sessionId}/end_vote`)
+      xhr.post(`${state.sessionId}/end_vote`)
         .then(({ data }) => {
+          console.log('end vote', data);
           commit('setVoteActive', false);
+          commit('setVoteCompleted', true);
           commit('setVoteStats', data.statistics);
         })
     },
-    sendVote({ commit, state }, payload) {
+    sendVote({ commit, state, dispatch }, payload) {
       xhr.post(`${state.sessionId}/vote`, {
         user_name: state.playerName,
         vote_value: payload
       })
         .then(response => {
           console.log('send vote resp', response);
+          dispatch('getAllVotes');
           commit('setVoteSet');
         })
-    }
+    },
+    startNewVote({ commit, state }, payload) {
+      xhr.post(`${state.sessionId}/new_vote`, {
+        title: payload
+      })
+        .then(response => {
+          console.log('send start new vote resp', response);
+          commit('clearVoteStats', true);
+          commit('setVoteActive', true);
+          commit('setVoteCompleted', false);
+        })
+        .catch(() => {
+          commit('setVoteActive', false);
+          commit('setVoteCompleted', false);
+        })
+    },
+    getSessionInfo({ commit, state, dispatch }) {
+      xhr.get(`${state.sessionId}/info`)
+        .then(({ data }) => {
+          console.log('session info', data);
+
+          commit('setRoomName', data.session_name);
+          commit('setSessionId', data.session_id);
+          commit('setPlayersList', data.session_users);
+
+          if (data.active_sessions.length){
+            commit('setVoteActive', true);
+            commit('setActiveVoteTitle', data.active_sessions[0]);
+            dispatch('getAllVotes');
+          }
+        })
+        .catch(response => {
+          console.log('session info error', response);
+        })
+    },
   },
   modules: {
   }
